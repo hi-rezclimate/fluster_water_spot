19bfbcf139e32d6e35e59e7b45be419290518d6a
Shoichi Otomo <p.chang.0129@gmail.com>
2015-04-12 04:43:34 +0900

    rename dir

---
+++ root_map.js
@@ -0,0 +1,103 @@
+$( function(){
+	var rendererOptions = {
+	  draggable: false
+	};
+
+	var directionsDisplay = new google.maps.DirectionsRenderer( rendererOptions );;
+	var directionsService = new google.maps.DirectionsService();
+	var map;
+	var markers = [];
+
+	var wells;
+
+	var Tokyo = new google.maps.LatLng( 35.41032, 139.44982 );
+
+	google.maps.event.addDomListener( window, 'load', initialize );
+
+	$( "#place_form" ).submit(function(){
+		calcRoute( $( "#place_text" ).val() );
+    return false;
+	})
+
+	function initialize() {
+
+	  var mapOptions = {
+	    zoom: 3,
+	    center: Tokyo
+	  };
+	  map = new google.maps.Map( document.getElementById('map-canvas'), mapOptions );
+	  var fluster = new Fluster2( map );
+
+		$.getJSON(
+		  'data/well-uniq.geojson',
+		  function(data) {
+		    for( var i in data.features ){
+		    	var marker = new google.maps.Marker({
+		    		position: new google.maps.LatLng(
+		    				data.features[ i*30 ].properties.lat,
+		    				data.features[ i*30 ].properties.lng
+		    			),
+		    		title: "井戸"
+		    	});
+		    	fluster.addMarker( marker );
+		    	markers.push( marker );
+		    	if( i > 1000 ) break;
+		    };
+		  }
+		);
+
+		$.getJSON(
+		  'data/drink-water_uniq.geojson',
+		  function(data) {
+		    for( var i in data.features ){
+		    	var marker = new google.maps.Marker({
+		    		position: new google.maps.LatLng(
+		    				data.features[ i*30 ].properties.lat,
+		    				data.features[ i*30 ].properties.lng
+		    			),
+		    		title: "蛇口"
+		    	});
+		    	fluster.addMarker( marker );
+		    	markers.push( marker );
+		    	if( i > 2000 ) break;
+		    };
+		  }
+		);
+
+		$.getJSON(
+		  'data/dam_japan.geojson',
+		  function(data) {
+		    for( var i in data.features ){
+		    	var marker = new google.maps.Marker({
+		    		position: new google.maps.LatLng(
+		    				data.features[ i*30 ].properties.lat,
+		    				data.features[ i*30 ].properties.lng
+		    			),
+		    		title: "ダム"
+		    	});
+		    	fluster.addMarker( marker );
+		    	markers.push( marker );
+		    	if( i > 100 ) break;
+		    };
+		  }
+		);
+		fluster.initialize();
+
+		google.maps.event.addListener( map, 'idle', function(){
+			fluster.showClustersInBounds();
+		});
+	}
+
+	function calcRoute( from ) {
+	  var request = {
+	    origin: from,
+	    destination: '渋谷',
+	    travelMode: google.maps.TravelMode.DRIVING
+	  };
+	  directionsService.route( request, function( response, status ) {
+	    if ( status == google.maps.DirectionsStatus.OK ) {
+	      directionsDisplay.setDirections( response );
+	    }
+	  });
+	}
+} );
